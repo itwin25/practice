1734d908639da5b792cd0e5000497db40474baec
itwin25 <toto0887@gmail.com>
2025-09-04 17:25:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 77 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int ans;
+	static int[][] month;
+	static int[] fee;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
 		for (int test_case = 1; test_case <= T; test_case++) {
-			int[] fee = new int[4]; // 요금
-			int[][] month = new int[13][2]; // 월별 사용일[0], 요금[1]
-			int ans = 0;
+			fee = new int[4]; // 요금
+			month = new int[13][2]; // 월별 사용일[0], 요금[1]
 
 			// 요금 입력
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -25,7 +29,9 @@ public class Solution {
 				month[i][0] = Integer.parseInt(st.nextToken());
 			}
 
+			// 1일? 한 달?
 			for (int i = 1; i < 13; i++) {
+				// 1일 요금제 > 한 달 요금제
 				if (month[i][0] * fee[0] > fee[1]) {
 					month[i][1] = fee[1];
 				} else {
@@ -33,21 +39,24 @@ public class Solution {
 				}
 			}
 
-			int[] dp = new int[13];
+			ans = fee[3];
+			int totalCost = 0;
 
-			dp[1] = month[1][1];
-			dp[2] = dp[1] + month[2][1];
-
-			for (int i = 3; i < 13; i++) {
-				dp[i] = Math.min(dp[i - 1] + month[i][1], dp[i - 3] + fee[2]);
-			}
-
-			if (dp[12] >= fee[3]) {
-				System.out.println("#" + test_case + " " + fee[3]);
-			} else {
-				System.out.println("#" + test_case + " " + dp[12]);
-			}
-
+			dfs(1, totalCost);
+			System.out.println("#" + test_case + " " + ans);
 		} // tc
 	} // main
+
+	private static void dfs(int curr, int totalCost) {
+		if (curr >= 13) {
+			ans = Math.min(totalCost, ans);
+			return;
+		}
+		if (totalCost >= ans) {
+			return;
+		}
+
+		dfs(curr + 1, totalCost + month[curr][1]);
+		dfs(curr + 3, totalCost + fee[2]);
+	}
 }
